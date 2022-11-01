6e192c7daca3f3d89206d08c88660d56837eb474
wenanlin <675355804@qq.com>
2022-11-01 20:25:43 +0800

    sonar

--- src/main/java/Helloword.kt
+++ src/main/java/Helloword.kt
@@ -1,4 +1,13 @@
 package src.main.java
 
 class Helloword {
+
+
+    override fun toString(): String {
+        return super.toString()
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+    }
 }
